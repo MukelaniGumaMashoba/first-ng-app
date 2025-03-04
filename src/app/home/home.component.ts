750f316474997ddf2d44ea3a84f103eb4fb515d2
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-03-04 21:45:25 +0200

    new components and new design on the home page aka Recaps

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../components/greeting/greeting.component';
 import { CounterComponent } from '../components/counter/counter.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent, CounterComponent],
+  imports: [GreetingComponent, CounterComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
  message = signal('Welcome to the home page!');
+
+ content = signal('This is the content of the home page');
+
  keyUpHnadler(event: KeyboardEvent) {
    console.log(`Key ${event.key} event was triggered`);
  }
